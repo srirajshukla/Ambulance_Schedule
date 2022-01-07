25e8ba9e9221945b92cc275df4842d1c0efd643a
Gyanendra Shukla <srirajshukla@gmail.com>
2022-01-07 19:11:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/gyan/ambulanceschedule/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gyan.ambulanceschedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
