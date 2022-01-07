5d86fb484b7f4b101d130543310b246dd1ab9691
Gyanendra Shukla <srirajshukla@gmail.com>
2022-01-07 20:26:44 +0530

    add schedule model

---
+++ app/src/main/java/com/gyan/ambulanceschedule/data/Schedule.kt
@@ -0,0 +1,21 @@
+package com.gyan.ambulanceschedule.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Schedule (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name="name")
+    val name: String,
+    @ColumnInfo(name="phone")
+    val phone: String,
+    @ColumnInfo(name="startTime")
+    val startTime: String,
+    @ColumnInfo(name="endTime")
+    val endTime : String,
+    @ColumnInfo(name="day")
+    val day: String,
+)
